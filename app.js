21e4487fa5f66a216eccfa63fa4dceb6b927743b
Oleksii Demchenko <41051912+alexDemche@users.noreply.github.com>
2019-12-09 14:58:33 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+.state('app.promotions-local-v2', {
+                page_name: 'promotions-local-v2',
+                url: '/promotions-local-v2',
+                templateUrl: '/app/templates/pages/promotions-local-v2.html'
+            })
